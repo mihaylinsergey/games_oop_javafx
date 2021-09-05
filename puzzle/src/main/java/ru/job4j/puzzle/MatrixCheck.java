6cb6833569c4419d988ffec910668d1bce655325
mihaylinsergey <semi999@mail.ru>
2021-09-05 12:41:03 +0300

    2. Сокобан. Тестовое задание. [#75283]

---
+++ puzzle/src/main/java/ru/job4j/puzzle/MatrixCheck.java
@@ -0,0 +1,25 @@
+package ru.job4j.puzzle;
+
+public class MatrixCheck {
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,13 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (MatrixCheck.monoHorizontal(board, i) || MatrixCheck.monoVertical(board, i))) {
+                rsl = true;
+                break;
+            }
+        }
         return rsl;
     }
 }
